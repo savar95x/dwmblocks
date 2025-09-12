1264f0ab76777c4aa021dfd3f5f043c31292c5cc
savar <skaar513@gmail.com>
2025-09-12 18:15:25 +0530

    organising

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 #define CONFIG_H
 
 // String used to delimit block outputs in the status.
-#define DELIMITER "  "
+#define DELIMITER "     "
 
 // Maximum number of Unicode characters that a block can output.
 #define MAX_BLOCK_OUTPUT_LENGTH 200
@@ -20,11 +20,13 @@
 // Define blocks for the status feed as X(icon, cmd, interval, signal).
 #define BLOCKS(X)             \
     X("", "~/.config/dwm/scripts/music.sh", 5, 1) \
-    X("", "~/.config/dwm/scripts/light.sh", 0, 13) \
-    X("", "~/.config/dwm/scripts/audio.sh", 0, 10) \
-    X("", "~/.config/dwm/scripts/battery.sh", 0, 12) \
+    X("", "~/.config/dwm/scripts/misc.sh", 0, 0) \
+    X("", "~/.config/dwm/scripts/redshift.sh", 30, 21) \
     X("", "~/.config/dwm/scripts/wifi.sh", 30, 16) \
-    X("", "~/.config/dwm/scripts/date.sh", 60, 17)
+    X("", "~/.config/dwm/scripts/battery.sh", 30, 12) \
+    X("", "~/.config/dwm/scripts/search.sh", 0, 20) \
+    X("", "~/.config/dwm/scripts/date.sh", 60, 17) \
+    X("", "~/.config/dwm/scripts/padding.sh", 0, 20)
 
 
 #endif  // CONFIG_H
